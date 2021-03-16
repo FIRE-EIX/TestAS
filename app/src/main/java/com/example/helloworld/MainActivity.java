3976b34f9819c98a15c701ecd8b44d820180c4ac
FIRE_EIX <632126598@qq.com>
2021-03-16 08:09:58 +0800

    add Log information

--- app/src/main/java/com/example/helloworld/MainActivity.java
+++ app/src/main/java/com/example/helloworld/MainActivity.java
@@ -1,14 +1,41 @@
 package com.example.helloworld;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
+
+    private static final String TAG = "LogTest";
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG, "----- onStart() method called -----");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "----- onDestroy() method called -----");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG, "----- onStop() method called -----");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG, "----- onResume() method called -----");
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d(TAG, "----- onCreate() method called -----");
     }
 }
